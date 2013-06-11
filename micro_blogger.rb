fa5943999a8b78d81ddb935f336d171f1f76dd7c
unicornzero <rylee.k@gmail.com>
2013-06-11 12:58:40 -0600

    added initial tweet functionality

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,10 +1,16 @@
 require 'jumpstart_auth'
 
 class MicroBlogger
-	attr_reader :client
+  attr_reader :client
 
-	def initialize
+  def initialize
     puts "Initializing..."
     @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+    @client.update(message)
+  end
 end
+
+blogger = MicroBlogger.new
