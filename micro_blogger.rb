9d262451beae91e3e65d1c3ebac77eafc15f9394
unicornzero <rylee.k@gmail.com>
2013-06-12 12:45:00 -0600

    added klout query

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,9 @@
 require 'jumpstart_auth'
+require 'bitly'
+require 'klout'
+
+Bitly.use_api_version_3
+
 
 class MicroBlogger
   attr_reader :client, :followers
@@ -8,10 +13,17 @@ class MicroBlogger
     @client = JumpstartAuth.twitter
     @followers = []
     getfollowers
+    Klout.api_key = 'xu9ztgnacmjx3bu82warbr3h'
+
   end
 
   def getfollowers
     @followers = @client.followers.collect{|follower| follower.screen_name}
+  end
+
+  def shorten(url)
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    return bitly.shorten(url).short_url
   end
 
   def tweet(message)
@@ -46,6 +58,17 @@ class MicroBlogger
     end
   end
 
+  def klout_score
+    friends = @client.friends.collect{|f| f.screen_name}
+    friends.each do |friend|
+      puts "#{friend} has a Klout score of:"
+      identity = Klout::Identity.find_by_screen_name(friend)
+      user = Klout::User.new(identity.id)
+      puts user.score.score
+      puts "" 
+    end
+  end
+
   def run
     puts "Welcome to the TinMan Twitter Client!"
     command = ""
@@ -57,9 +80,11 @@ class MicroBlogger
       case command
         when 'q' then puts "Goodbye!"
         when 't' then tweet(parts[1..-1].join(" "))
+        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
         when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         when 'spam' then spam_my_friends(parts[1..-1].join(" "))
         when 'buzz' then everyones_last_tweet
+        when 'klout' then klout_score
         else
           puts "Sorry, I don't know how to #{command}"
       end
@@ -70,3 +95,4 @@ end
 
 blogger = MicroBlogger.new
 blogger.run
+
